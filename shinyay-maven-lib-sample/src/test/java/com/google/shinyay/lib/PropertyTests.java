10250e9e28ab4f90de9fcd42f13c63ad32e05dc8
shinyay <shinya.com@gmail.com>
2021-05-16 07:40:06 +0900

    Add Spring Boot Application annotated static class for testing

--- shinyay-maven-lib-sample/src/test/java/com/google/shinyay/lib/PropertyTests.java
+++ shinyay-maven-lib-sample/src/test/java/com/google/shinyay/lib/PropertyTests.java
@@ -1,13 +1,22 @@
 package com.google.shinyay.lib;
 
+import com.google.shinyay.lib.service.SampleService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(value = "sample.message=Hello")
 class PropertyTests {
 
+	@Autowired
+	private SampleService service;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@SpringBootApplication
+	static class TestProperty{}
 }
