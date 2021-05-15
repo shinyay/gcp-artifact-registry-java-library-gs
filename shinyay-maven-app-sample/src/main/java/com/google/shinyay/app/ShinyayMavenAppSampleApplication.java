c3849123e6559157dc8cb3ad64a5308e23b01d1c
shinyay <shinya.com@gmail.com>
2021-05-16 08:25:18 +0900

    Modify spring bean scann base package

--- shinyay-maven-app-sample/src/main/java/com/google/shinyay/app/ShinyayMavenAppSampleApplication.java
+++ shinyay-maven-app-sample/src/main/java/com/google/shinyay/app/ShinyayMavenAppSampleApplication.java
@@ -3,7 +3,7 @@ package com.google.shinyay.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.google.shinyay")
 public class ShinyayMavenAppSampleApplication {
 
 	public static void main(String[] args) {
