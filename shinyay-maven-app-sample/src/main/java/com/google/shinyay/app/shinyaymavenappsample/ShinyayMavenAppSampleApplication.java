f7b3c85763aafbd18591e0a8ce23131483553123
shinyay <shinya.com@gmail.com>
2021-05-16 08:10:35 +0900

    Add sample app for using user library from Artifact Registry

---
+++ shinyay-maven-app-sample/src/main/java/com/google/shinyay/app/shinyaymavenappsample/ShinyayMavenAppSampleApplication.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.app.shinyaymavenappsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyayMavenAppSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyayMavenAppSampleApplication.class, args);
+	}
+
+}
---
+++ shinyay-maven-app-sample/src/test/java/com/google/shinyay/app/shinyaymavenappsample/ShinyayMavenAppSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.app.shinyaymavenappsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyayMavenAppSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
