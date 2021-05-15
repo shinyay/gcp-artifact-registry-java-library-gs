580bad9b319cba0348036408e677bdb1ee6bb116
shinyay <shinya.com@gmail.com>
2021-05-16 07:28:39 +0900

    Add registration for Property Configuration class

--- shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/SampleService.java
+++ shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/SampleService.java
@@ -1,7 +1,9 @@
 package com.google.shinyay.lib.service;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableConfigurationProperties(SampleServiceProperties.class)
 public class SampleService {
 }
