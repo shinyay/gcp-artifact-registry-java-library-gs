f85b44feb3cc106bbc064bf17e6f32b00dbb8ee5
shinyay <shinya.com@gmail.com>
2021-05-15 10:43:04 +0900

    Add ConfigurationProperties annotated class for application properties

---
+++ shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/ShinyaService.java
@@ -0,0 +1,2 @@
+package com.google.shinyay.lib.service;public class ShinyaService {
+}
---
+++ shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/ShinyaServiceProperties.java
@@ -0,0 +1,7 @@
+package com.google.shinyay.lib.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("shinyay")
+public class ShinyaServiceProperties {
+}
