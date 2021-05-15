beef5eecfa31806fc7f5ea040c755c21f029b2cf
shinyay <shinya.com@gmail.com>
2021-05-16 07:36:51 +0900

    Add application property value

--- shinyay-maven-lib-sample/src/test/java/com/google/shinyay/lib/ShinyayMavenLibSampleApplicationTests.java
+++ shinyay-maven-lib-sample/src/test/java/com/google/shinyay/lib/ShinyayMavenLibSampleApplicationTests.java
@@ -3,9 +3,9 @@ package com.google.shinyay.lib;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ShinyayMavenLibSampleApplicationTests {
-
+@SpringBootTest(value = "sample.message=Hello")
+class PropertyTests {
+	
 	@Test
 	void contextLoads() {
 	}
