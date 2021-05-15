a065ed5508f42f3647659806f8b8b103cb07cf45
shinyay <shinya.com@gmail.com>
2021-05-16 08:25:45 +0900

    Add display message method

--- shinyay-maven-app-sample/src/main/java/com/google/shinyay/app/controller/SampleController.java
+++ shinyay-maven-app-sample/src/main/java/com/google/shinyay/app/controller/SampleController.java
@@ -1,5 +1,20 @@
 package com.google.shinyay.app.controller;
 
+import com.google.shinyay.lib.service.SampleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SampleController {
+
+    private final SampleService service;
+
+    public SampleController(SampleService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String homeMessage() {
+        return service.message();
+    }
 }
