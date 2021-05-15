d035eb65018660e24c20dc2ecff43d474c9b02cb
shinyay <shinya.com@gmail.com>
2021-05-15 11:16:06 +0900

    Add new library project

---
+++ shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/ShinyayMavenLibSampleApplication.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyayMavenLibSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyayMavenLibSampleApplication.class, args);
+	}
+
+}
---
+++ shinyay-maven-lib-sample/src/test/java/com/google/shinyay/lib/ShinyayMavenLibSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyayMavenLibSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
