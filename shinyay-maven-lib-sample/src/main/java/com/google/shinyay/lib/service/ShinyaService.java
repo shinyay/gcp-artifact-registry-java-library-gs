c898b300d6f4af0725af179599c05ae570654b98
shinyay <shinya.com@gmail.com>
2021-05-15 10:50:38 +0900

    Add service component using application properties

--- shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/ShinyaService.java
+++ shinyay-maven-lib-sample/src/main/java/com/google/shinyay/lib/service/ShinyaService.java
@@ -1,2 +1,9 @@
-package com.google.shinyay.lib.service;public class ShinyaService {
+package com.google.shinyay.lib.service;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableConfigurationProperties(ShinyaServiceProperties.class)
+public class ShinyaService {
 }
