724691846274fe2004783373251b07cba285a8db
shinyay <shinya.com@gmail.com>
2021-05-17 06:33:50 +0900

    Add gradle library project

---
+++ shinyay-gradle-lib-sample/src/main/java/com/google/shinyay/lib/ShinyayGradleLibSampleApplication.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyayGradleLibSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyayGradleLibSampleApplication.class, args);
+	}
+
+}
---
+++ shinyay-gradle-lib-sample/src/test/java/com/google/shinyay/lib/ShinyayGradleLibSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.shinyay.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyayGradleLibSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
