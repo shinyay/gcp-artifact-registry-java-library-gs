9dfad1f56706a2a263ab0a08ba7597520d1e854b
shinyay <shinya.com@gmail.com>
2021-05-17 09:17:02 +0900

    Add static class for SpringBootApplication to test

--- shinyay-gradle-lib-sample/src/test/java/com/google/shinyay/lib/PropertyTests.java
+++ shinyay-gradle-lib-sample/src/test/java/com/google/shinyay/lib/PropertyTests.java
@@ -1,13 +1,24 @@
 package com.google.shinyay.lib;
 
+import com.google.shinyay.lib.service.SampleService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(value = "sample.message=Hello")
 class PropertyTests {
+
+	@Autowired
+	private SampleService service;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(service.message()).isNotNull();
 	}
+
+	@SpringBootApplication
+	static class TestProperty{}
 
 }
