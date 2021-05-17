837430f9fd3ac5769d3867627c4f80be00b66992
shinyay <shinya.com@gmail.com>
2021-05-17 09:02:18 +0900

    Add application property class for sample property

---
+++ shinyay-gradle-lib-sample/src/main/java/com/google/shinyay/lib/service/SampleServiceProperties.java
@@ -0,0 +1,7 @@
+package com.google.shinyay.lib.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("sample")
+public class SampleServiceProperties {
+}
